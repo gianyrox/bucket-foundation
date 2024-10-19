0da9537701550af8903a257f128f1228f3cc0e60
eugene <gianyrox@gmail.com>
2024-10-18 23:00:07 -0400

    library

---
+++ src/app/library/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+export default function Page() {
+
+  const research_name = "research name"
+  const open_function = () => { }
+  const cite_function = () => { }
+
+  return (
+    <div className=" flex justify-center items-center">
+
+      <div className="w-[96%] h-32 flex bg-teal-950 border-teal-800 border-2 rounded-xl justify-between items-center flex-row px-4">
+        <text className="text-4xl font-bold">{research_name}</text>
+        <div className="flex gap-4">
+          <button
+            className="bg-green-950 border-green-800 border-2 rounded-lg p-4"
+            onClick={open_function}>
+            <text className="text-2xl font-bold">
+              Open
+            </text>
+          </button>
+          <button
+            className="bg-amber-950 border-amber-800 border-2 rounded-lg p-4"
+            onClick={cite_function}>
+            <text className="text-2xl font-bold">
+              Cite
+            </text>
+          </button>
+        </div>
+
+      </div>
+    </div>
+  );
+}
